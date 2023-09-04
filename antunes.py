255e077afb9c197ecdb37b5b7e2393adc2fdec92
antuneswz <144069256+antuneswz@users.noreply.github.com>
2023-09-04 19:29:17 -0300

    Add files via upload

---
+++ antunes.py
@@ -0,0 +1,57 @@
+#while True:
+   #menu=int(input('1:cadastro<2:login,3:fechar'))
+
+    #if(menu==):
+        #print('cadastro')
+    #elif(menu==2):
+        #print('login')
+    #elif(menu==3):
+        #print('o menu fechará')
+
+ #print('fim de cadastro')
+#nome='pablo'
+#idade=26
+
+#print('seu nome é',nome,'e sua idade é',idade,'.')
+#print(f'seu nome é(nome) e sua idade é(idade).')
+
+#print(f'''
+
+    # DADOS DOS USARIOSS
+
+
+#''')
+    
+
+#idade=int(input('diga sua idade'))
+#idade+int(input('diga sua altura'))
+#altura=float(input('diga sua altura'))
+
+#print(f'''
+#seu nome é {nome}
+#sua idade é {idade}
+#sua altura é {altura:.2f}''')
+
+    
+#numero1=float(input('diga o numero:'))
+#numero2=float(input('diga o segundo numero:'))
+
+#print(f'''
+#sua altura é{numero1/numero2:.1f}''')
+
+
+
+
+lista=[]
+
+while true:
+    x=input('''
+1- adicionar itema
+2-remover item
+3-visualizar lista
+4-sair''')
+    if(x==1):
+    elif(x==2):
+    elif(x==3):
+    elif(x==4):
+else:
